5e8c72ec7d1174f343148c646a9a61da15e21f7e
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-08-21 23:44:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity) {
+  if (activity === undefined) {
+    return "This Saturday, I want to roller-skate!"
+  } else {
+    return 'This Saturday, I want to bathe my dog!';
+  }
+}
+function mondayWork(activity) {
+  if (activity === undefined) {
+    return "This Monday, I will go to the office.";
+  } else {
+    return "This Monday, I will work from home.";
+  }
+}
+function wrapAdjective(adjective) {
+  return function(string) {
+    if (adjective === '*') {
+      return "You are *a hard worker*!";
+    } else {
+        return "You are ||a dedicated programmer||!";
+        }
+  }
+}
+
